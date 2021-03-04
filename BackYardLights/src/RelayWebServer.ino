ced5c15e0da61abf23ddf73c154bc13cecfb5ffd
Richard Steenburg <rsteenbu@gmail.com>
2021-03-03 21:19:03 -0800

    fix frontyard timing bug and new backyard project

--- BackYardLights/src/RelayWebServer.ino
+++ BackYardLights/src/RelayWebServer.ino
@@ -5,6 +5,15 @@
 #include <ArduinoOTA.h>
 #include <ArduinoJson.h>
 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_VEML7700.h>
+#include <OneWire.h>
+
+#include <Adafruit_AM2315.h>
+Adafruit_AM2315 am2315;
+
 #include <my_relay.h>
 
 #include <time.h>                       // time() ctime()
@@ -18,15 +27,17 @@
 // specify the port to listen on as an argument
 WiFiServer server(80);
 
-// This device info
-#define APP_NAME "switch"
-#define JSON_SIZE 200
+#define JSON_SIZE 300
+#define SYSTEM_APPNAME "system"
+#define LIGHT_APPNAME "lightsensor"
+#define THERMO_APPNAME "thermometer"
+#define LIGHTSWITCH_APPNAME "lightswitch"
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP udpClient;
 
 // Create a new syslog instance with LOG_LOCAL0 facility
-Syslog syslog(udpClient, SYSLOG_SERVER, SYSLOG_PORT, DEVICE_HOSTNAME, APP_NAME, LOG_LOCAL0);
+Syslog syslog(udpClient, SYSLOG_SERVER, SYSLOG_PORT, DEVICE_HOSTNAME, SYSTEM_APPNAME, LOG_LOCAL0);
 
 int debug = 0;
 
@@ -36,17 +47,18 @@ const int RX_PIN=3;
 const int GPIO0_PIN=0;
 const int GPIO2_PIN=2;
 
-
 char msg[40];
 StaticJsonDocument<200> doc;
-Relay xMasLights(TX_PIN);
+Relay lvLights(TX_PIN);
+
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Booting up");
 
   // Setup the Relay
-  xMasLights.setup();
+  lvLights.setup();
 
   // Connect to WiFi network
   WiFi.mode(WIFI_STA);
@@ -76,14 +88,72 @@ void setup() {
 
   // Start the server
   server.begin();
+
+  // set I2C pins (SDA, SDL)
+  Wire.begin(GPIO2_PIN, GPIO0_PIN);
+
+  syslog.appName(THERMO_APPNAME);
+  if (! am2315.begin()) {
+    syslog.log(LOG_INFO, "AM23315 Sensor not found");
+  }
+
+  syslog.appName(LIGHT_APPNAME);
+  if (!veml.begin()) {
+    syslog.log(LOG_INFO, "VEML Sensor not found");
+  } else {
+    veml.setGain(VEML7700_GAIN_1);
+    veml.setIntegrationTime(VEML7700_IT_800MS);
+
+    switch (veml.getGain()) {
+      case VEML7700_GAIN_1: syslog.logf(LOG_INFO, "Gain: %s", "1"); break;
+      case VEML7700_GAIN_2: syslog.logf(LOG_INFO, "Gain: %s", "2"); break;
+      case VEML7700_GAIN_1_4: syslog.logf(LOG_INFO, "Gain: %s", "1/4"); break;
+      case VEML7700_GAIN_1_8: syslog.logf(LOG_INFO, "Gain: %s", "1/8"); break;
+    }
+
+    Serial.print(F("Integration Time (ms): "));
+    switch (veml.getIntegrationTime()) {
+      case VEML7700_IT_25MS: syslog.logf(LOG_INFO, "Integration Time (ms): %d", 25); break;
+      case VEML7700_IT_50MS: syslog.logf(LOG_INFO, "Integration Time (ms): %d", 50); break;
+      case VEML7700_IT_100MS: syslog.logf(LOG_INFO, "Integration Time (ms): %d", 100); break;
+      case VEML7700_IT_200MS: syslog.logf(LOG_INFO, "Integration Time (ms): %d", 200); break;
+      case VEML7700_IT_400MS: syslog.logf(LOG_INFO, "Integration Time (ms): %d", 400); break;
+      case VEML7700_IT_800MS: syslog.logf(LOG_INFO, "Integration Time (ms): %d", 800); break;
+    }
+    veml.setLowThreshold(10000);
+    veml.setHighThreshold(20000);
+    veml.interruptEnable(false);
+  }
+
 }
 
 static time_t now;
+int16_t lightLevel = 0;
+int prevSeconds = 0;;
+float temperature, humidity = -1;
 void loop() {
   ArduinoOTA.handle();
   //gettimeofday(&tv, nullptr);
   //clock_gettime(0, &tp);
   now = time(nullptr);
+    
+  int currSeconds = localtime(&now)->tm_sec;
+  if ( currSeconds != prevSeconds ) {
+    if ( debug ) {
+      syslog.logf(LOG_INFO, "seconds changed, now: %d, prev: %d", currSeconds, prevSeconds);
+    }
+    if ( currSeconds % 5 == 0 ) {
+      lightLevel = veml.readLux();
+      syslog.appName(THERMO_APPNAME);
+      if (! am2315.readTemperatureAndHumidity(&temperature, &humidity)) {
+	syslog.log(LOG_INFO, "Failed to read data from AM2315");
+      }
+      // convert the temperature to F
+      temperature = ((temperature * 9) / 5 + 32);
+    }
+  } 
+
+  prevSeconds = currSeconds;
 
   // Check if a client has connected
   WiFiClient client = server.available();
@@ -113,25 +183,28 @@ void loop() {
     syslog.log(LOG_INFO, "Debug level 2");
     debug = 2;
   } else if (req.indexOf(F("/light/status")) != -1) {
-    client.print(xMasLights.on);
+    client.print(lvLights.on);
   } else if (req.indexOf(F("/light/off")) != -1) {
       syslog.log(LOG_INFO, "Turning light off");
-      xMasLights.switchOff();
+      lvLights.switchOff();
   } else if (req.indexOf(F("/light/on")) != -1) {
       syslog.log(LOG_INFO, "Turning light on");
-      xMasLights.switchOn();
+      lvLights.switchOn();
   } else if (req.indexOf(F("/status")) != -1) {
     StaticJsonDocument<JSON_SIZE> doc;
     JsonObject switches = doc.createNestedObject("switches");
-    switches["light"]["state"] = xMasLights.state();
+    switches["light"]["state"] = lvLights.state();
+    JsonObject sensors = doc.createNestedObject("sensors");
+    sensors["lightLevel"] = lightLevel;
+    sensors["temperature"] = temperature;
+    sensors["humidity"] = humidity;
     doc["debug"] = debug;
     doc["ctime"] = ctime(&now);
     doc["seconds"] = localtime(&now)->tm_sec;
 
-    size_t jsonDocSize = measureJsonPretty(doc);
-    if (jsonDocSize > JSON_SIZE) {
-      client.printf("ERROR: JSON message too long, %d", jsonDocSize);
-      client.println();
+    if (measureJsonPretty(doc) > JSON_SIZE) {
+      client.println("ERROR: JSON message too long");
+      syslog.log(LOG_INFO, "JSON message too long");
     } else {
       serializeJsonPretty(doc, client);
       client.println();
