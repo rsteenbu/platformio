e5fb7b53006481a7a640ee7ea9c0fdf36bcdf77d
Richard Steenburg <rsteenbu@gmail.com>
2021-03-25 22:07:01 -0700

    working irrigation timer relay

--- lib/relay/my_relay.h
+++ lib/relay/my_relay.h
@@ -5,6 +5,7 @@
 #include <sys/time.h>                   // struct timeval
 #include <coredecls.h>                  // settimeofday_cb()
 #include <TZ.h>
+#include <Array.h>
 
 class Relay {
   int pin;
@@ -31,8 +32,11 @@ class Relay {
     time_t onTime = 0;
     time_t offTime = 0;
 
-    void setScheduleOverride(bool s) {
-      scheduleOverride = s;
+    void setBackwards(bool a) {
+      backwards = a;
+    }
+    void setScheduleOverride(bool a) {
+      scheduleOverride = a;
     }
     bool getScheduleOverride() {
       return scheduleOverride;
@@ -68,23 +72,91 @@ class Relay {
 
 class TimerRelay: public Relay {
   public:
-    int runTime;
+    int runTime = 0;
     int startHour;
-    int startmin;
-    int startDays[8];
+    int startMinute;
+    Array<int,7> runDays;
+
     TimerRelay(int a): Relay(a) {}
 
     void setRuntime(int a) {
       runTime = a;
     }
     
-    void setStartTime(int a, int b, int c[8]) {
+    void setStartTime(int a, int b) {
       startHour = a;
-      startmin = b;
+      startMinute = b;
     }
 
-    void handle() {
-    }
+   void setEveryOtherDayOn() {
+     for ( int n=0 ; n<7 ; n++ ) {
+       if (n % 2 == 0) {
+	 runDays[n] = 1;
+       } else {
+	 runDays[n] = 0;
+       }
+     }
+   }
+
+   void setEveryDayOn() {
+     for ( int n=0 ; n<7 ; n++ ) {
+       runDays[n] = 1;
+     }
+   }
+
+   void setEveryDayOff() {
+     for ( int n=0 ; n<7 ; n++ ) {
+       runDays[n] = 0;
+     }
+   }
+
+   void setDaysFromArray(Array<int,7> & array) {
+     for ( int n=0 ; n<7 ; n++ ) {
+       runDays[n] = array[n];
+     }
+   } 
+
+   void setSpecificDayOn(int n) {
+     runDays[n] = 1;
+   }
+
+   int checkToRun(int weekDay) { 
+     return runDays[weekDay];
+   } 
+
+   int handle() {
+     time_t now = time(nullptr);
+     struct tm *timeinfo = localtime(&now);
+     int currHour = timeinfo->tm_hour;
+     int currMinute = timeinfo->tm_min;
+     int weekDay = timeinfo->tm_wday;
+
+     int currMinuteOfDay = (currHour * 60) + currMinute;
+     int startMinuteOfDay = (startHour * 60) + startMinute;
+
+     if ( runTime == 0 ) {
+       return 0;
+     }
+
+     // if we're not on, turn it on if it's the right day and time
+     if (!on) {
+       if ( runDays[weekDay] && currMinuteOfDay == startMinuteOfDay )
+       {
+	 switchOn();
+	 return 1;
+       } else {
+	 return 0;
+       }
+     // if we're on, turn it off if it's been more than than the time to run
+     } else {
+       if ( now >= onTime + (runTime * 60)  ) {
+	 switchOff();
+	 return 2;
+       }
+     }
+     return 0;
+   }
+
 };
 
 
