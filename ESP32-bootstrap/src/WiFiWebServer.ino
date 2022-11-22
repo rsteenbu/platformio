dbd71e7e967f1fca5160b5ca7b477374628c2511
Richard Steenburg <rsteenbu@gmail.com>
2022-11-21 18:33:44 -0800

    removing secrets

--- ESP32-bootstrap/src/WiFiWebServer.ino
+++ ESP32-bootstrap/src/WiFiWebServer.ino
@@ -5,9 +5,6 @@
 #include <Syslog.h>
 #include <ezTime.h>
 
-// WiFi credentials.
-const char* WIFI_SSID = "***REMOVED***";
-const char* WIFI_PASS = "***REMOVED***";
 const char* SYSLOG_SERVER = "ardupi4";
 const int SYSLOG_PORT = 514;
 const char* DEVICE_HOSTNAME = "iot-esp32bootstrap";
